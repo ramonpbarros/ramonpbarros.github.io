410937865a33079ecbcd12f31c15069ca55e8820
Sorrab <ramonpbarros@gmail.com>
2020-07-08 18:19:47 -0700

    navbar implemented

--- app.js
+++ app.js
@@ -8,3 +8,25 @@ for (let i = 0; i < logo.length; i++) {
 // particlesJS.load('particles-js', 'assets/particles.json', function() {
 //   console.log('callback - particles.js config loaded');
 // });
+const themeMap = {
+  dark: "light",
+  light: "solar",
+  solar: "dark"
+};
+
+const theme = localStorage.getItem('theme')
+  || (tmp = Object.keys(themeMap)[0],
+      localStorage.setItem('theme', tmp),
+      tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem('theme');
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem('theme', next);
+}
+
+document.getElementById('themeButton').onclick = toggleTheme;
