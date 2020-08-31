c9ac32c78411cd00181ec6a3b10d34144aab9898
Sorrab <ramonpbarros@gmail.com>
2020-08-31 14:50:06 -0700

    fixed margin on contact btn

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -54,7 +54,7 @@ particlesJS("particles-js", {
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 80, duration: 0.4 },
+      repulse: { distance: 80, duration: 0.8 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
     }
