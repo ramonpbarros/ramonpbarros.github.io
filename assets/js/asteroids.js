1964f27535f10b212a9ac6fc8cf55b01c59cd607
Sorrab <ramonpbarros@gmail.com>
2020-09-03 15:02:41 -0700

    created asteroids

--- assets/js/asteroids.js
+++ assets/js/asteroids.js
@@ -76,6 +76,8 @@ class Ship {
     ctx.beginPath();
     let vertAngle = ((Math.PI * 2) / 3);
     let radians = this.angle / Math.PI * 180;
+    this.noseX = this.x - this.radius * Math.cos(radians);
+    this.noseY = this.y - this.radius * Math.sin(radians);
     for(let i = 0; i < 3; i++) {
       ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
     }
@@ -87,7 +89,62 @@ class Ship {
 class Bullet {
   constructor(angle) {
     this.visible = true;
-    this.x = 
+    this.x = ship.noseX;
+    this.y = ship.noseY;
+    this.angle = angle;
+    this.height = 4;
+    this.width = 4;
+    this.speed = 5;
+    this.velX = 0;
+    this.velY = 0;
+  }
+  Update() {
+    let radians = this.angle / Math.PI * 180;
+    this.x -= Math.cos(radians) * this.speed;
+    this.y -= Math.sin(radians) * this.speed;
+  }
+  Draw() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+class Asteroid {
+  constructor(x, y) {
+    this.visible = true;
+    this.x = Math.floor(Math.random() * canvasWidth);
+    this.y = Math.floor(Math.random() * canvasHeight);
+    this.speed = 1;
+    this.radius = 50;
+    this.angle = Math.floor(Math.random() * 359);
+    this.strokeColor = 'white';
+  }
+  Update() {
+    let radians = this.angle / Math.PI * 180;
+    this.x += Math.cos(radians) * this.speed;
+    this.y += Math.sin(radians) * this.speed;
+    if(this.x < this.radius) {
+      this.x = canvas.width;
+    }
+    if(this.x > this.width) {
+      this.x = this.radius;
+    }
+    if(this.y < this.radius) {
+      this.y = canvas.height;
+    }
+    if(this.x > this.height) {
+      this.x = this.radius;
+    }
+  }
+  Draw() {
+    ctx.beginPath();
+    let vertAngle = ((Math.PI * 2) / 6);
+    let radians = this.angle / Math.Pi * 180;
+    for(let i = 0; i < 6; i++) {
+      ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
+    }
+    ctx.closePath();
+    ctx.stroke();
   }
 }
 
