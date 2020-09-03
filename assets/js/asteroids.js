4a5fa44937fc4b9d5b74bec6a0cba58b1315a1ff
Sorrab <ramonpbarros@gmail.com>
2020-09-03 14:44:29 -0700

    create ship and movement

--- assets/js/asteroids.js
+++ assets/js/asteroids.js
@@ -32,12 +32,64 @@ class Ship {
     this.velX = 0;
     this.velY = 0;
     this.rotateSpeed = 0.001;
-    
+    this.radius = 15;
+    this.angle = 0;
+    this.strokeColor = 'white';
+  }
+  Rotate(dir) {
+    this.angle += this.rotateSpeed * dir;
+  }
+  Update() {
+    let radians = this. angle / Math.PI * 180;
+    if(this.movingForward) {
+      //oldX + cos(radians) * distance
+      this.velX += Math.cos (radians) * this.speed;
+      //oldY + sin(radians) * distance
+      this.velY += Math.sin (radians) * this.speed;
+    }
+    if(this.x < this.radius) {
+      this.x = canvas.width;
+    }
+    if(this.x > this.width) {
+      this.x = this.radius;
+    }
+    if(this.y < this.radius) {
+      this.y = canvas.height;
+    }
+    if(this.x > this.height) {
+      this.x = this.radius;
+    }
+    this.velX *= 0.99;
+    this.velY *= 0.99;
+
+    this.x -= this.velX;
+    this.y -= this.velY;
+  }
+  Draw() {
+    ctx.strokeStyle = this.strokeColor;
+    ctx.beginPath();
+    let vertAngle = ((Math.PI * 2) / 3);
+    let radians = this.angle / Math.PI * 180;
+    for(let i = 0; i < 3; i++) {
+      ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
+    }
+    ctx.closePath();
+    ctx.stroke();
   }
 }
 
 let ship = new Ship();
 
 function Render() {
-
+  ship.movingForward = (keys[87]);
+  if(keys[68]){
+    ship.Rotate(1);
+  }
+  if(keys[65]){
+    ship.Rotate(-1);
+  }
+  ctx.clearRect(0,0,canvasWidth,canvasHeight);
+  ship.Update();
+  ship.Draw();
+  requestAnimationFrame(Render);
 }
