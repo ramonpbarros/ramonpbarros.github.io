0f6834ddbf266169e535e6c931cbec19ebaab5e0
Sorrab <ramonpbarros@gmail.com>
2020-09-03 14:14:13 -0700

    created files for asteroid game

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
-const logo = document.querySelectorAll("#gallery path");
+// const logo = document.getElementById('logo');
+// const content = document.getElementById('content');
+// let state = false;
 
-for (let i = 0; i < logo.length; i++) {
-  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-// particlesJS.load('particles-js', 'assets/particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-// });
-// const themeMap = {
-//   dark: "light",
-//   light: "solar",
-//   solar: "dark"
-// };
-
-// const theme = localStorage.getItem('theme')
-//   || (tmp = Object.keys(themeMap)[0],
-//       localStorage.setItem('theme', tmp),
-//       tmp);
-// const bodyClass = document.body.classList;
-// bodyClass.add(theme);
-
-// function toggleTheme() {
-//   const current = localStorage.getItem('theme');
-//   const next = themeMap[current];
-
-//   bodyClass.replace(current, next);
-//   localStorage.setItem('theme', next);
+// for (let i = 0; i < logo.length; i++) {
+//   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 // }
 
-// document.getElementById('themeButton').onclick = toggleTheme;
+// $(document).keypress(function(e){
+//   let key = e.which;
+//   if (key == 32 && state == false) {
+//     logo.style.visibility = "hidden";
+//     content.style.visibility = "hidden";
+//     state = true;
+//   } else if (key == 32 && state == true) {
+//     logo.style.visibility = "visible";
+//     content.style.visibility = "visible";
+//     state = false;
+//   }
+// });
+
---
+++ assets/js/asteroids.js
@@ -0,0 +1,43 @@
+let canvas;
+let ctx;
+let canvasWidth = 1400;
+let canvasHeight = 1000;
+let keys = [];
+
+document.addEventListener('DOMContentLoaded', SetupCanvas);
+
+function SetupCanvas() {
+  canvas = document.getElementById('my-canvas');
+  ctx = canvas.getContext('2d');
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+  document.body.addEventListener("keydown", function(e){
+    keys[e.keyCode] = true;
+  });
+  document.body.addEventListener("keyup", function(e){
+    keys[e.keyCode] = false;
+  });
+  Render();
+}
+
+class Ship {
+  constructor() {
+    this.visible = true;
+    this.x = canvasWidth / 2;
+    this.y = canvasHeight / 2;
+    this.movingForward = false;
+    this.speed = 0.1;
+    this.velX = 0;
+    this.velY = 0;
+    this.rotateSpeed = 0.001;
+    
+  }
+}
+
+let ship = new Ship();
+
+function Render() {
+
+}
