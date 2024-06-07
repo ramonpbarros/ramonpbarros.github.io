cce62f8dc53a74608c41c6e85d453c179a65985a
Ramon <ramonpbarros@gmail.com>
2024-06-06 21:40:09 -0700

    cursor modified

--- assets/js/asteroids.js
+++ assets/js/asteroids.js
@@ -17,43 +17,43 @@ function SetupCanvas() {
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
   ctx.fillStyle = 'black';
-  ctx.fillRect(0,0,canvas.width, canvas.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   ship = new Ship();
 
-  for(let i = 0; i < 8; i++) {
+  for (let i = 0; i < 8; i++) {
     asteroids.push(new Asteroid());
   }
 
   setTimeout(function () {
-    for(let i = 0; i < 4; i++) {
+    for (let i = 0; i < 4; i++) {
       asteroids.push(new Asteroid());
     }
   }, 8000);
 
   setTimeout(function () {
-    for(let i = 0; i < 8; i++) {
+    for (let i = 0; i < 8; i++) {
       asteroids.push(new Asteroid());
     }
   }, 16000);
 
   setTimeout(function () {
-    for(let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
       asteroids.push(new Asteroid());
     }
   }, 24000);
 
   setTimeout(function () {
-    for(let i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++) {
       asteroids.push(new Asteroid());
     }
   }, 32000);
 
-  document.body.addEventListener("keydown", function(e){
+  document.body.addEventListener('keydown', function (e) {
     keys[e.keyCode] = true;
   });
-  document.body.addEventListener("keyup", function(e){
+  document.body.addEventListener('keyup', function (e) {
     keys[e.keyCode] = false;
-    if(e.keyCode === 32) {
+    if (e.keyCode === 32) {
       bullets.push(new Bullet(ship.angle));
     }
   });
@@ -80,23 +80,23 @@ class Ship {
     this.angle += this.rotateSpeed * dir;
   }
   Update() {
-    let radians = this. angle / Math.PI * 180;
-    if(this.movingForward) {
+    let radians = (this.angle / Math.PI) * 180;
+    if (this.movingForward) {
       //oldX + cos(radians) * distance
-      this.velX += Math.cos (radians) * this.speed;
+      this.velX += Math.cos(radians) * this.speed;
       //oldY + sin(radians) * distance
-      this.velY += Math.sin (radians) * this.speed;
+      this.velY += Math.sin(radians) * this.speed;
     }
-    if(this.x < this.radius) {
+    if (this.x < this.radius) {
       this.x = canvas.width;
     }
-    if(this.x > this.width) {
+    if (this.x > this.width) {
       this.x = this.radius;
     }
-    if(this.y < this.radius) {
+    if (this.y < this.radius) {
       this.y = canvas.height;
     }
-    if(this.y > this.height) {
+    if (this.y > this.height) {
       this.y = this.radius;
     }
     this.velX *= 0.99;
@@ -108,12 +108,15 @@ class Ship {
   Draw() {
     ctx.strokeStyle = this.strokeColor;
     ctx.beginPath();
-    let vertAngle = ((Math.PI * 2) / 3);
-    let radians = this.angle / Math.PI * 180;
+    let vertAngle = (Math.PI * 2) / 3;
+    let radians = (this.angle / Math.PI) * 180;
     this.noseX = this.x - this.radius * Math.cos(radians);
     this.noseY = this.y - this.radius * Math.sin(radians);
-    for(let i = 0; i < 3; i++) {
-      ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
+    for (let i = 0; i < 3; i++) {
+      ctx.lineTo(
+        this.x - this.radius * Math.cos(vertAngle * i + radians),
+        this.y - this.radius * Math.sin(vertAngle * i + radians)
+      );
     }
     ctx.closePath();
     ctx.stroke();
@@ -133,7 +136,7 @@ class Bullet {
     this.velY = 0;
   }
   Update() {
-    let radians = this.angle / Math.PI * 180;
+    let radians = (this.angle / Math.PI) * 180;
     this.x -= Math.cos(radians) * this.speed;
     this.y -= Math.sin(radians) * this.speed;
   }
@@ -156,29 +159,31 @@ class Asteroid {
     this.level = level || 1;
   }
   Update() {
-    let radians = this.angle / Math.PI * 180;
+    let radians = (this.angle / Math.PI) * 180;
     this.x += Math.cos(radians) * this.speed;
     this.y += Math.sin(radians) * this.speed;
-    if(this.x < this.radius) {
+    if (this.x < this.radius) {
       this.x = canvas.width;
     }
-    if(this.x > this.width) {
+    if (this.x > this.width) {
       this.x = this.radius;
     }
-    if(this.y < this.radius) {
+    if (this.y < this.radius) {
       this.y = canvas.height;
     }
-    if(this.y > this.height) {
+    if (this.y > this.height) {
       this.y = this.radius;
     }
   }
   Draw() {
     ctx.beginPath();
-    let vertAngle = ((Math.PI * 2) / 6);
-    let radians = this.angle / Math.PI * 180;
-    for(let i = 0; i < 6; i++) {
-      ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), 
-      this.y - this.radius * Math.sin(vertAngle * i + radians));
+    let vertAngle = (Math.PI * 2) / 6;
+    let radians = (this.angle / Math.PI) * 180;
+    for (let i = 0; i < 6; i++) {
+      ctx.lineTo(
+        this.x - this.radius * Math.cos(vertAngle * i + radians),
+        this.y - this.radius * Math.sin(vertAngle * i + radians)
+      );
     }
     ctx.closePath();
     ctx.stroke();
@@ -192,7 +197,7 @@ function CircleCollision(p1x, p1y, r1, p2x, p2y, r2) {
   radiusSum = r1 + r2;
   xDiff = p1x - p2x;
   yDiff = p1y - p2y;
-  if(radiusSum > Math.sqrt((xDiff * xDiff) + (yDiff * yDiff))) {
+  if (radiusSum > Math.sqrt(xDiff * xDiff + yDiff * yDiff)) {
     return true;
   } else {
     return false;
@@ -202,12 +207,15 @@ function CircleCollision(p1x, p1y, r1, p2x, p2y, r2) {
 function DrawLifeShips() {
   let startX = 1350;
   let startY = 10;
-  let points = [[9,9], [-9,9]];
+  let points = [
+    [9, 9],
+    [-9, 9],
+  ];
   ctx.strokeStyle = 'red';
   for (let i = 0; i < lives; i++) {
     ctx.beginPath();
     ctx.moveTo(startX, startY);
-    for(let j = 0; j < points.length; j++) {
+    for (let j = 0; j < points.length; j++) {
       ctx.lineTo(startX + points[j][0], startY + points[j][1]);
     }
     ctx.closePath();
@@ -217,14 +225,14 @@ function DrawLifeShips() {
 }
 
 function Render() {
-  ship.movingForward = (keys[87]);
-  if(keys[68]){
+  ship.movingForward = keys[87];
+  if (keys[68]) {
     ship.Rotate(1);
   }
-  if(keys[65]){
+  if (keys[65]) {
     ship.Rotate(-1);
   }
-  ctx.clearRect(0,0,canvasWidth,canvasHeight);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   ctx.fillStyle = 'white';
   ctx.font = '21px Arial';
   ctx.fillText('SCORE: ' + score.toString(), 20, 35);
@@ -232,24 +240,33 @@ function Render() {
   ctx.fillText('Press Spacebar to shoot', 20, 690);
   ctx.fillText('A,W,D to control ship', 20, 710);
   ctx.fillText('ESC to exit game', 20, 730);
-  if(lives <= 0) {
+  if (lives <= 0) {
     ship.visible = false;
     ctx.fillStyle = 'red';
     ctx.font = '50px Arial';
     ctx.fillText('GAME OVER', canvasWidth / 2 - 150, canvasHeight / 2);
     setTimeout(function () {
-      window.location.href = "./index.html";
+      window.location.href = './index.html';
     }, 4000);
   }
 
-  if(keys[27]){
-    window.location.href = "./index.html";
+  if (keys[27]) {
+    window.location.href = './index.html';
   }
   DrawLifeShips();
 
-  if(asteroids.length !== 0) {
-    for(let k = 0; k < asteroids.length; k++) {
-      if(CircleCollision(ship.x, ship.y, 11, asteroids[k].x, asteroids[k].y, asteroids[k].collisionRadius)){
+  if (asteroids.length !== 0) {
+    for (let k = 0; k < asteroids.length; k++) {
+      if (
+        CircleCollision(
+          ship.x,
+          ship.y,
+          11,
+          asteroids[k].x,
+          asteroids[k].y,
+          asteroids[k].collisionRadius
+        )
+      ) {
         ship.x = canvasWidth / 2;
         ship.y = canvasHeight / 2;
         ship.velX = 0;
@@ -259,24 +276,36 @@ function Render() {
     }
   }
 
-  if(asteroids.length !== 0 && bullets.length != 0) {
-loop1:
-    for(let l = 0; l < asteroids.length; l++){
-      for(let m = 0; m < bullets.length; m++) {
-        if(CircleCollision(bullets[m].x, bullets[m].y, 3, asteroids[l].x, asteroids[l].y, asteroids[l].collisionRadius)) {
-          if(asteroids[l].level === 1) {
-            asteroids.push(new Asteroid(asteroids[l].x - 5,
-              asteroids[l].y - 5, 25, 2, 22));
-            asteroids.push(new Asteroid(asteroids[l].x + 5,
-              asteroids[l].y + 5, 25, 2, 22));
-          } else if(asteroids[l].level === 2) {
-            asteroids.push(new Asteroid(asteroids[l].x - 5,
-              asteroids[l].y - 5, 15, 3, 12));
-            asteroids.push(new Asteroid(asteroids[l].x + 5,
-              asteroids[l].y + 5, 15, 3, 12));
+  if (asteroids.length !== 0 && bullets.length != 0) {
+    loop1: for (let l = 0; l < asteroids.length; l++) {
+      for (let m = 0; m < bullets.length; m++) {
+        if (
+          CircleCollision(
+            bullets[m].x,
+            bullets[m].y,
+            3,
+            asteroids[l].x,
+            asteroids[l].y,
+            asteroids[l].collisionRadius
+          )
+        ) {
+          if (asteroids[l].level === 1) {
+            asteroids.push(
+              new Asteroid(asteroids[l].x - 5, asteroids[l].y - 5, 25, 2, 22)
+            );
+            asteroids.push(
+              new Asteroid(asteroids[l].x + 5, asteroids[l].y + 5, 25, 2, 22)
+            );
+          } else if (asteroids[l].level === 2) {
+            asteroids.push(
+              new Asteroid(asteroids[l].x - 5, asteroids[l].y - 5, 15, 3, 12)
+            );
+            asteroids.push(
+              new Asteroid(asteroids[l].x + 5, asteroids[l].y + 5, 15, 3, 12)
+            );
           }
-          asteroids.splice(l,1);
-          bullets.splice(m,1);
+          asteroids.splice(l, 1);
+          bullets.splice(m, 1);
           score += 20;
           break loop1;
         }
@@ -284,18 +313,18 @@ loop1:
     }
   }
 
-  if(ship.visible) {
+  if (ship.visible) {
     ship.Update();
     ship.Draw();
   }
 
-  if(bullets.length !== 0) {
+  if (bullets.length !== 0) {
     for (let i = 0; i < bullets.length; i++) {
       bullets[i].Update();
       bullets[i].Draw();
     }
   }
-  if(asteroids.length !== 0) {
+  if (asteroids.length !== 0) {
     for (let j = 0; j < asteroids.length; j++) {
       asteroids[j].Update();
       asteroids[j].Draw(j);
--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,63 +1,63 @@
-particlesJS("particles-js", {
+particlesJS('particles-js', {
   particles: {
     number: {
       value: 80,
       density: {
         enable: true,
-        value_area: 800
-      }
+        value_area: 800,
+      },
     },
-    color: { value: "#ffffff" },
+    color: { value: '#ffffff' },
     shape: {
-      type: "circle",
+      type: 'circle',
       stroke: {
         width: 0,
-        color: "#030303"
+        color: '#030303',
       },
-      polygon: { nb_sides: 5 }
+      polygon: { nb_sides: 5 },
     },
     opacity: {
       value: 0.5,
       random: false,
-      anim: { enable: false, speed: 2, opacity_min: 0.1, sync: false }
+      anim: { enable: false, speed: 2, opacity_min: 0.1, sync: false },
     },
     size: {
       value: 2,
       random: true,
-      anim: { enable: false, speed: 0, size_min: 0.1, sync: false }
+      anim: { enable: false, speed: 0, size_min: 0.1, sync: false },
     },
     line_linked: {
       enable: false,
       distance: 150,
-      color: "#030303",
+      color: '#030303',
       opacity: 0.4,
-      width: 1
+      width: 1,
     },
     move: {
       enable: true,
       speed: 1,
-      direction: "random",
+      direction: 'random',
       random: true,
       straight: false,
-      out_mode: "out",
+      out_mode: 'out',
       bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 }
-    }
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
   },
   interactivity: {
-    detect_on: "window",
+    detect_on: 'window',
     events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: false, mode: "push" },
-      resize: true
+      onhover: { enable: true, mode: 'repulse' },
+      onclick: { enable: false, mode: 'push' },
+      resize: true,
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
       repulse: { distance: 80, duration: 0.8 },
       push: { particles_nb: 4 },
-      remove: { particles_nb: 2 }
-    }
+      remove: { particles_nb: 2 },
+    },
   },
-  retina_detect: false
+  retina_detect: false,
 });
